89d9d1d961c60bd167576cabb6d8911dbf52a6ca
Muskan <muskanchhatrasal@gmail.com>
2021-05-28 18:40:38 +0530

    JSX - Javascript

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import ReactDOM from 'react-dom';
 //CSS
 import './index.css';
 
-//Nested Components
 
+const author = 'Robert T. Kiyosaki';
+const title = 'Rich Dad Poor Dad';
 function BookList(){
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   )
@@ -21,17 +18,11 @@ function BookList(){
 const Book = () =>{
   return (
       <article className='book'>
-        <Image />
-        <Title />
-        <Author />
+        <img src='https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg' alt='' />
+        <h1>{title}</h1>
+        <h3>{author}</h3>
       </article>
   )
 }
-
-const Image = () => <img src='https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg' alt='' />
-
-const Title = () => <h1>Rich Dad Poor Dad</h1>
-
-const Author = () => <h3 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>Robert T. Kiyosaki</h3>
 
 ReactDOM.render(<BookList/>,document.getElementById('root'));
