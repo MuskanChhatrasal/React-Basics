0af6cae4ee9b3810a040de20d1feac521b75c772
Muskan <muskanchhatrasal@gmail.com>
2021-05-28 20:06:31 +0530

    Event Basics

--- src/index.js
+++ src/index.js
@@ -37,11 +37,15 @@ function BookList(){
 
 const Book = (props) =>{
   const {img,title,author} = props;
+  const clickHandler = () =>{
+    alert ('Hello World');
+  }
   return (
       <article className='book'>
         <img src={img} alt='' />
         <h1>{title}</h1>
         <h3>{author}</h3>
+        <button type='button' onClick={clickHandler}>Refrence Example</button>
       </article>
   )
 }
