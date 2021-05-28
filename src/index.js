081e43aa09f0bc091518015ad4a0f7dabeaed806
Muskan <muskanchhatrasal@gmail.com>
2021-05-28 19:02:24 +0530

    Props

--- src/index.js
+++ src/index.js
@@ -4,23 +4,32 @@ import ReactDOM from 'react-dom';
 //CSS
 import './index.css';
 
+const firstBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg',
+  title: 'Rich Dad Poor Dad',
+  author: 'Robert T. Kiyosaki',
+}
+const secondBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
+  title: 'Ikigai',
+  author: 'Héctor García',
+}
 
-const author = 'Robert T. Kiyosaki';
-const title = 'Rich Dad Poor Dad';
 function BookList(){
   return (
     <section className="bookList">
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   )
 }
 
-const Book = () =>{
+const Book = (props) =>{
   return (
       <article className='book'>
-        <img src='https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg' alt='' />
-        <h1>{title}</h1>
-        <h3>{author}</h3>
+        <img src={props.img} alt='' />
+        <h1>{props.title}</h1>
+        <h3>{props.author}</h3>
       </article>
   )
 }
